73a9e22cea989ca8fd56745a298225b879774817
JoksanBahena <20203tn008@utez.edu.mx>
2024-10-30 13:13:00 -0600

    [FIX] Arreglo de estilos

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
 
   return (
     <aside className="sidebar w-64 bg-gray-100 h-full p-4 font-[family-name:var(--font-jost-medium)]">
-      <h6 className="font-regular mb-4">
+      <h6 className="mb-4">
         Hola Furro,
         <p className="font-[family-name:var(--font-jost-bold)]">¿Qué quieres hacer hoy?</p>
       </h6>
--- src/app/_components/title.tsx
+++ src/app/_components/title.tsx
@@ -1,6 +1,6 @@
 const Title = ({ title }: { title: string }) => {
   return (
-    <h1 className="text-xl font-[family-name:var(--font-jost-medium)]">
+    <h1 className="text-xl font-[family-name:var(--font-jost-medium)] text-primary">
       {title}
     </h1>
   );
