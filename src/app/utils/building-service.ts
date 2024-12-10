8a5723d9bd8ce88408d34c41c6380bd06c04674f
DavidRos1231 <20213tn038@utez.edu.mx>
2024-12-10 17:02:48 -0600

    Creacion de edificios y cuartos

--- src/app/utils/building-service.ts
+++ src/app/utils/building-service.ts
@@ -1,6 +1,6 @@
-const URL_BASE = process.env.URL_BASE;
-
 "use server";
+
+const URL_BASE = process.env.URL_BASE;
 
 export const getBuildings = async () => {
   const response = await fetch(`${URL_BASE}/dashboard/getAll`, {
@@ -142,3 +142,95 @@ export const changeStatusBuilding = async (buildingId: string) => {
 
 	return response.json();
 };
+
+export const changeStatusRoom = async (roomId: string, status: string) => {
+	const response = await fetch(`${URL_BASE}/room/change-status`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			id: roomId,
+			newStatus: status,
+		}),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al cambiar el estado de la habitación');
+	}
+
+	return response.json();
+};
+
+
+export const createBuilding = async (name: string, floors: number) => {
+	const response = await fetch(`${URL_BASE}/building/create`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			name: name,
+		}),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al crear el edificio');
+	}
+
+	const theResponse = await response.json();
+	const buildingId = theResponse.data.id;
+
+	const floorsArray = [];
+	for (let i = 0; i < floors; i++) {
+		const newFloor = {
+			name: `piso ${i + 1}`,
+			buildingId: buildingId,
+		};
+		floorsArray.push(newFloor);
+	}
+
+	const response2 = await fetch(`${URL_BASE}/floor/createList`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(
+			floorsArray
+		),
+	});
+
+	if (!response2.ok) {
+		throw new Error('Error al crear los pisos');
+	}
+
+	return response2.json();
+};
+
+export const createRoomsList = async (floorId: string, rooms: number) => {
+	const roomsArray = [];
+	for (let i = 0; i < rooms; i++) {
+		const newRoom = {
+			identifier: `habitación ${i + 1}`,
+			name: `habitación ${i + 1}`,
+			floorId: floorId,
+		};
+		roomsArray.push(newRoom);
+	}
+
+	const response = await fetch(`${URL_BASE}/room/createList`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(
+			roomsArray
+		),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al crear las habitaciones');
+	}
+
+	return response.json();
+}
