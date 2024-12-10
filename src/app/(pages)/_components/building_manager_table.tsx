ea38c854b1eee81132d1f6c2099c1b453807fd62
DavidRos1231 <20213tn038@utez.edu.mx>
2024-12-10 15:34:19 -0600

    prieros cambios manager

--- src/app/(pages)/_components/building_manager_table.tsx
+++ src/app/(pages)/_components/building_manager_table.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import { ApartmentOutlined, DeleteOutlineOutlined, EditOutlined, RestoreFromTrashOutlined } from '@mui/icons-material';
 import Link from 'next/link';
 import { BuildingDashboard } from "@/app/types/BuildingDashboard"; // Importa el tipo si no está
+import { changeStatusBuilding } from "@/app/utils/building-service";
+import { Toast } from '@/app/lib/toast';
+import build from 'next/dist/build';
+const handleStatusBuilding = async (buildingId: string, fetchBuildings:(category:string)=>void,clickedCategory:string) => {
+    try {
+        const response= await changeStatusBuilding(buildingId);
+        if (response.statusCode === 200) {
+            Toast.fire({
+                icon: "success",
+                title: "Edificio actualizado correctamente",
+              });        
+                fetchBuildings(clickedCategory);
+            } else {
+                Toast.fire({
+                    icon: "error",
+                    title: "Error al actualizar el edificio",
+                  });       
+                 }
+    } catch (error) {
+        console.error("Error changing building status:", error);
+    }
+}
 
-const BuildingManagerTable = ({ buildings }: { buildings: BuildingDashboard[] }) => {
+const BuildingManagerTable = ({ buildings, fetchBuildings, clickedCategory }: { buildings: any[]; fetchBuildings: (category: string) => void; clickedCategory: string}) => {
     return (
         <table className="w-full border-collapse">
             <thead>
@@ -18,39 +40,52 @@ const BuildingManagerTable = ({ buildings }: { buildings: BuildingDashboard[] })
                 </tr>
             </thead>
             <tbody>
+            {buildings.length === 0 ? (
+                <tr>
+                    <td colSpan={7} className="py-3 px-4 text-center">No hay edificios disponibles</td>
+                </tr>
+            ) : null}
                 {buildings.map((building, index) => (
-                    <tr key={building.building.id} className="border-b border-gray-200">
+                    <tr key={building.id} className="border-b border-gray-200">
                         <td className="py-3 px-4">{index + 1}</td>
                         <td className="py-3 px-4 flex items-center gap-2 text-primary font-[family-name:var(--font-jost-medium)]">
                             <button className="p-2 bg-complementary rounded-full">
                                 <ApartmentOutlined className="text-background" />
                             </button>
                             <Link href={`/manager/buildings/rooms`} className="hover:underline">
-                                {building.building.name}
+                                {building.name}
                             </Link>
                         </td>
                         <td className="py-3 px-4 text-orange-500 text-center">{building.reportedRooms}</td>
                         <td className="py-3 px-4 text-error text-center">{building.disabledRooms}</td>
                         <td className="py-3 px-4 text-center">{building.totalRooms}</td>
                         <td className="py-3 px-4 flex gap-2">
-                            {building.building.status ? (
+                            {building.status ? (
                                 <>
-                                    <button className="p-2 border-2 border-primary rounded-full" aria-label={`Editar edificio ${building.building.name}`}>
+                                    <button className="p-2 border-2 border-primary rounded-full" aria-label={`Editar edificio ${building.name}`}>
                                         <EditOutlined className="text-primary" />
                                     </button>
-                                    <button className="p-2 border-2 border-error rounded-full" aria-label={`Eliminar edificio ${building.building.name}`}>
+                                    <button 
+                                        className="p-2 border-2 border-error rounded-full" 
+                                        aria-label={`Eliminar edificio ${building.name}`} 
+                                        onClick={() => handleStatusBuilding(building.id, fetchBuildings, clickedCategory)}
+                                    >
                                         <DeleteOutlineOutlined className="text-error" />
                                     </button>
                                 </>
                             ) : (
-                                <button className="p-2 border-2 border-secondary rounded-full" aria-label={`Restaurar edificio ${building.building.name}`}>
+                                <button 
+                                className="p-2 border-2 border-secondary rounded-full" 
+                                aria-label={`Restaurar edificio ${building.name}`}
+                                onClick={() => handleStatusBuilding(building.id, fetchBuildings, clickedCategory)}
+                                >
                                     <RestoreFromTrashOutlined className="text-secondary" />
                                 </button>
                             )}
                         </td>
                         <td className="py-3 px-4">
-                            <button className={`py-1 px-4 rounded-lg ${building.building.status ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}>
-                                {building.building.status ? 'Activo' : 'Inactivo'}
+                            <button className={`py-1 px-4 rounded-lg ${building.status ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}>
+                                {building.status ? 'Activo' : 'Inactivo'}
                             </button>
                         </td>
                     </tr>
--- src/app/utils/building-service.ts
+++ src/app/utils/building-service.ts
@@ -10,7 +10,51 @@ export const getBuildings = async () => {
 
 	if (!response.ok) {
 		throw new Error('Error al obtener los edificios');
-	}
+	};
+
+	return response.json();
+};
+
+export const getBuildingsBuildingApi = async () => {
+	const response = await fetch(`${URL_BASE}/building/getAll`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al obtener los edificios');
+	};
+
+	return response.json();
+};
+
+export const getBuildingsActive = async () => {
+	const response = await fetch(`${URL_BASE}/building/getAllActive`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al obtener los edificios');
+	};
+
+	return response.json();
+};
+export const getBuildingsInactive = async () => {
+	const response = await fetch(`${URL_BASE}/building/getAllInactive`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al obtener los edificios');
+	};
 
 	return response.json();
 };
@@ -52,3 +96,93 @@ export const changeStatusRoom = async (roomId: string, status: string) => {
 
 	return response.json();
 };
+
+export const changeStatusBuilding = async (buildingId: string) => {
+	const response = await fetch(`${URL_BASE}/building/changeStatus/${buildingId}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			id: buildingId,
+		}),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al cambiar el estado del edificio');
+	}
+
+	return response.json();
+}
+
+export const createBuilding = async (name: string, floors: number) => {
+	const response = await fetch(`${URL_BASE}/building/create`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			name: name,
+		}),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al crear el edificio');
+	}
+
+	const theResponse = await response.json();
+	const buildingId = theResponse.data.id;
+
+	const floorsArray = [];
+	for (let i = 0; i < floors; i++) {
+		const newFloor = {
+			name: `piso ${i + 1}`,
+			buildingId: buildingId,
+		};
+		floorsArray.push(newFloor);
+	}
+
+	const response2 = await fetch(`${URL_BASE}/floor/createList`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(
+			floorsArray
+		),
+	});
+
+	if (!response2.ok) {
+		throw new Error('Error al crear los pisos');
+	}
+
+	return response2.json();
+};
+
+export const createRoomsList = async (floorId: string, rooms: number) => {
+	const roomsArray = [];
+	for (let i = 0; i < rooms; i++) {
+		const newRoom = {
+			identifier: `habitación ${i + 1}`,
+			name: `habitación ${i + 1}`,
+			floorId: floorId,
+		};
+		roomsArray.push(newRoom);
+	}
+
+	const response = await fetch(`${URL_BASE}/room/createList`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(
+			roomsArray
+		),
+	});
+
+	if (!response.ok) {
+		throw new Error('Error al crear las habitaciones');
+	}
+
+	return response.json();
+}
