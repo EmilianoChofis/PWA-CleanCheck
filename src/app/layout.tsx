3a253eda8018ee7bba550a67049d5f184d9ef323
EmilianoChofis <20213tn128@utez.edu.mx>
2024-12-11 23:01:33 -0600

    instalacion de sw en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,12 @@ export default function RootLayout({
 
   useEffect(() => {
     setIsClient(true);
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => console.log('Service Worker registered:', registration))
+        .catch((error) => console.log('Service Worker registration failed:', error));
+    }
   }, []);
 
   const fontClasses = isClient
